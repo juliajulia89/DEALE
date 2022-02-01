67dff8c7e3d64c5ce7d24baf928a34a1add0dd2e
juliajulia89 <julkajanicka@gmail.com>
2022-02-01 15:54:36 +0100

    styling single beer card

--- src/components/BeerDetails.jsx
+++ src/components/BeerDetails.jsx
@@ -7,27 +7,23 @@ function SingleBeerDetails(props) {
     <div className="w-11/12 mx-auto p-2 border-2 rounded-md">
       <div className="w-full flex justify-center my-8">
         <img
-          className="object-contain w-1/3"
+          className="object-contain w-1/4"
           src={beerDetails[0].image_url}
           alt={beerDetails[0].name}
         />
-      </div>
-      <div className="flex justify-around my-4">
-        <div>
+
+        <div className="py-5 flex flex-col items-center justify-center space-y-1.5 w-3/4">
           <h1 className="text-lg text-black font-semibold">
             {beerDetails[0].name}
           </h1>
-          <p className="text-base font-medium text-gray-500">
-            {beerDetails[0].tagline}
-            <strong>Attenuation level:</strong> {beerDetails[0].attenuation_level}
-          </p>
-        </div>
-        <div className="flex flex-col justify-items-end">
-          <p className="text-base font-medium text-gray-500"></p>
-
+          <strong>Description:</strong>
           <p>{beerDetails[0].description}</p>
+          <strong>Food pairing:</strong>
           <p>{beerDetails[0].food_pairing}</p>
+          <strong>Brewer's tips:</strong>
           <p>{beerDetails[0].brewers_tips}</p>
+          <strong>Attenuation level:</strong>
+          <p>{beerDetails[0].attenuation_level}</p>
         </div>
       </div>
     </div>
