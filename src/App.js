c49acbf5835e459f8994234ac8d6acde4d4df8ee
juliajulia89 <julkajanicka@gmail.com>
2022-02-01 18:18:02 +0100

    add new beer attempt

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import HomePage from "./views/HomePage";
 import ListBeer from "./views/ListBeer";
 import SingleBeer from "./views/SingleBeer";
 import RandomBeer from "./views/RandomBeer";
-
+import NewBeer from "./views/NewBeer";
 
 function App() {
   return (
     <div className="App">
       <Switch>
+        <Route path="/new-beer" component={NewBeer} />
         <Route path="/random-beer" component={RandomBeer} />
         <Route path="/beers/:id" component={SingleBeer} />
         <Route path="/beers" component={ListBeer} />
