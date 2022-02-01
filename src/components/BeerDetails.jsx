551f6254b5eec02900c4c999de18240752b4dc47
juliajulia89 <julkajanicka@gmail.com>
2022-02-01 16:22:30 +0100

    styling beer detail page

--- src/components/BeerDetails.jsx
+++ src/components/BeerDetails.jsx
@@ -4,16 +4,16 @@ function SingleBeerDetails(props) {
   const { beerDetails } = props;
 
   return (
-    <div className="w-11/12 mx-auto p-2 border-2 rounded-md">
+    <div className="w-11/12 mx-auto p-2 border-2 rounded-md shadow shadow-lg my-4">
       <div className="w-full flex justify-center my-8">
         <img
-          className="object-contain w-1/4"
+          className="object-contain w-2/5"
           src={beerDetails[0].image_url}
           alt={beerDetails[0].name}
         />
 
-        <div className="py-5 flex flex-col items-center justify-center space-y-1.5 w-3/4">
-          <h1 className="text-lg text-black font-semibold">
+        <div className="py-5 flex flex-col items-center justify-center space-y-1.5 w-3/5">
+          <h1 className="text-lg text-black font-bold">
             {beerDetails[0].name}
           </h1>
           <strong>Description:</strong>
