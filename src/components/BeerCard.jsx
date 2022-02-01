6b5a59b53e3b0ca57c382368d3caca397c37171e
juliajulia89 <julkajanicka@gmail.com>
2022-02-01 13:49:08 +0100

    removes id from beerCard

--- src/components/BeerCard.jsx
+++ src/components/BeerCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function BeerCard({ beerDetails }) {
-  const { image_url, name, tagline, contributed_by, _id } = beerDetails;
+  const { image_url, name, tagline, contributed_by } = beerDetails;
 
   return (
     <Link to={`/beers/${beerDetails.id}`}>
